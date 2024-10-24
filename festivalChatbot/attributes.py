60219b0f5cedaf1163aa9ecd00fea50317904257
Saranath07 <saranathp34@gmail.com>
2024-10-24 16:30:40 +0530

    Initial chatbot

---
+++ festivalChatbot/attributes.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+attributes = [
+    'Name of the Festival',
+    'Date or Time of Year',
+    'Location',
+    'Historical Origins',
+    'Cultural or Religious Significance',
+    'Activities and Rituals',
+    'Cuisine',
+    'Attire and Decorations',
+    'Music and Dance',
+    'Participants',
+    'Duration',
+    'Preparation',
+    'Impact on Society',
+    'Modern Adaptations',
+    'Language and Expressions',
+    'Public Holidays',
+    'Mythological or Historical Figures',
+    'Inclusivity',
+    'Sustainability Practices',
+    'Emotional Significance'
+]
+
+festival_data = pd.DataFrame(columns=attributes)
